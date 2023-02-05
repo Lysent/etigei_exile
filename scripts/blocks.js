f7eca0bb5367c58863bfc8919383d3a938593153
Miter365 <miter360@gmail.com>
2023-02-05 15:13:41 -0500

    e

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -1,5 +1,4 @@
 Events.on(ContentInitEvent, e => {
-    Vars.content.block("gas-deposit").attributes.set(Attribute.get("gas-deposit"), 1);
+    Vars.content.block("etigeum-gas-deposit").attributes.set(Attribute.get("gas-deposit"), 1);
+    Vars.content.block("etigeum-fracker").attribute = Attribute.get("gas-deposit");
 });
-
-Vars.content.block("fracker").attribute = Attribute.get("gas-deposit");

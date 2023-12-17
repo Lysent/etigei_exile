bc8041254549bb9bca6707d2ac1978d53cfba4cf
Miter365 <miter360@gmail.com>
2023-12-17 01:03:08 -0500

    RW & Vanilla Update

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -1,6 +1,6 @@
 Events.on(ContentInitEvent, e => {
-    Vars.content.block("etigeum-gas-deposit").attributes.set(Attribute.get("gas"), 1);
+    Vars.content.block("etigeox-gas-deposit").attributes.set(Attribute.get("gas"), 1);
 
-    Vars.content.block("etigeum-fracker").attribute = Attribute.get("gas");
-    Vars.content.block("etigeum-hydrotherm").attribute = Attribute.get("gas");
+    Vars.content.block("etigeox-fracker").attribute = Attribute.get("gas");
+    Vars.content.block("etigeox-hydrotherm").attribute = Attribute.get("gas");
 });

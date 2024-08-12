131ec42aaf29ea78e98f99f2f91a0c059b7b7451
Miter365 <miter360@gmail.com>
2024-08-11 22:53:33 -0400

    Balance

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,14 @@ Blocks.coreShard.buildVisibility = BuildVisibility.shown;
 
 // Toxopid Build
 UnitTypes.toxopid.buildSpeed = 0.5;
+
+// Bullets
+const scrapBulletSalvo = extend(FlakBulletType, {
+    speed: 4,
+    lifetime: 65,
+    damage: 14,
+    shootEffect: Fx.shootSmall,
+    ammoMultiplier: 2,
+});
+
+Blocks.salvo.ammoTypes.put(Vars.content.item("scrap"), scrapBulletSalvo);

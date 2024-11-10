2844bf8226f57581179052b614d28912979c2164
superwibr <superwibr@gmail.com>
2024-11-10 17:17:07 -0500

    "sorter"

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
-require('./blocks.js');
-require('./bullets.js');
-require('./vanilla.js');
+require("./blocks.js");
+require("./bullets.js");
+require("./vanilla.js");
+require("./sorterthing.js");

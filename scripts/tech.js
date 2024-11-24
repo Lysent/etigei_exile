3ee100fc8f5cf292e62098b9bd7ca0f217988eb5
superwibr <superwibr@gmail.com>
2024-11-23 22:32:06 -0500

    logs & attempt

--- scripts/tech.js
+++ scripts/tech.js
@@ -2,21 +2,31 @@
 const addTechNode = (research) => {
     const node = new TechTree.TechNode(null, research.unlock, research.requirements || ItemStack.empty);
 
+    log("etigeox", "created tech node")
+
     if (research.planet) node.planet = Vars.content.getByName(ContentType.planet, research.planet);
 
+    log("etigeox", "updated planet")
+
     const parent = TechTree.all.find(t => t.content == research.parent && (t.planet !== null ? t.planet == node.planet : true));
+
+    log("etigeox", "(potentially) found parent")
 
     if (!parent.children.contains(node)) {
         parent.children.add(node);
     }
 
+    log("etigeox", "childed node")
+
     //reparent the node
     node.parent = parent;
+
+    log("etigeox", "reparented node")
 };
 
 addTechNode({
     parent: Vars.content.item("etigeox-refined-etigeum"),
-    unlock: Vars.content.item("etigeox-alumin"),
+    unlock: Vars.content.item("copper"),
     requirements: ItemStack.with(
         Items.copper, 100,
         Items.lead, 75

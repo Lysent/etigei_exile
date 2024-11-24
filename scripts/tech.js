6f4b3b9a43b4270e2e88661e1e3d40abc532f542
superwibr <superwibr@gmail.com>
2024-11-24 14:37:10 -0500

    node chaining

--- scripts/tech.js
+++ scripts/tech.js
@@ -1,27 +1,35 @@
 // Tech tree node attacher ripped off from the ContentParser
-const addTechNode = (research) => {
+const chainNode = (parent, research) => {
     const node = new TechTree.TechNode(null, research.unlock, research.requirements || ItemStack.empty);
 
-    log("etigeox", "created tech node")
+    log("etigeox", "created tech node");
 
     if (research.planet) node.planet = Vars.content.getByName(ContentType.planet, research.planet);
 
-    log("etigeox", "updated planet")
-
-    const parent = TechTree.all.find(t => t.content == research.parent && (t.planet !== null ? t.planet == node.planet : true));
-
-    log("etigeox", "(potentially) found parent")
+    log("etigeox", "updated planet");
 
     if (!parent.children.contains(node)) {
         parent.children.add(node);
     }
 
-    log("etigeox", "childed node")
+    log("etigeox", "childed node");
 
     //reparent the node
     node.parent = parent;
 
-    log("etigeox", "reparented node")
+    log("etigeox", "reparented node");
+
+    return node;
+};
+
+const addTechNode = (research) => {
+    const parent = TechTree.all.find(t => t.content == research.parent && (t.planet !== null ? t.planet == node.planet : true));
+
+    log("etigeox", "(potentially) found parent");
+
+    const node = chainNode(parent, research);
+
+    return node;
 };
 
 Events.on(ContentInitEvent, () => {
@@ -48,14 +56,13 @@ Events.on(ContentInitEvent, () => {
     // Nothing :/
 
     // Neoulandia
-    addTechNode({
+    const neoulandia_sand = addTechNode({
         parent: Vars.content.item("etigeox-timber"),
         unlock: Items.sand,
-        planet: "Neoulandia"
+        planet: "etigeox-Neoulandia"
     });
-    addTechNode({
-        parent: Items.sand,
+    chainNode(neoulandia_sand, {
         unlock: Items.silicon,
-        planet: "Neoulandia"
+        planet: "etigeox-Neoulandia"
     });
 });
